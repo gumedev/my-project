628ceb80b705845552261b21264c1dba1b4733d9
gume <gume.dev.mail+github@gmail.com>
2018-08-14 11:50:48 +0900

    #1 Add cssnano sample

--- script/build-css.sh
+++ script/build-css.sh
@@ -2,15 +2,18 @@
 
 Postcss_path=./node_modules/.bin/postcss
 Src_file=./src/css/style.css
-Dest_file=./src/css/style-dist.css
+Dest_file=./src/css/style.min.css
 
 function main(){
 
     local nested=postcss-nested
     local import=postcss-import
     local variable=postcss-simple-vars
+    local minify=cssnano
 
     # --useはarray
-    ${Postcss_path} --no-map --use ${import} ${nested} ${variable} -o ${Dest_file} ${Src_file}
+    ${Postcss_path} --no-map \
+		    --use ${import} ${nested} ${variable} ${minify} \
+		    -o ${Dest_file} ${Src_file}
 }
 main
