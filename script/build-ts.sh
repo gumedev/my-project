a9a19a431e18cb621807bd569c9906341b4ca0b9
gumedev <gume.mail.git@gmail.com>
2018-08-25 21:18:47 +0900

    #3 Add prettier-tslint sample

--- script/build-ts.sh
+++ script/build-ts.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+Package_json=./package.json
 Path_mocha=./node_modules/.bin/mocha
+Path_prettier=./node_modules/.bin/tslint
+
+function tslint(){
+
+    cat ${Package_json} | jq -r .tslintFile[] | while read tslint_file_line
+    do
+	${Path_prettier} --config ./tslint.json --fix ./src/ts/main/sample.ts ${tslint_file_line}
+    done
+}
+tslint
 
 function main(){
 
--- src/ts/test/test-sample.ts
+++ src/ts/test/test-sample.ts
@@ -1,22 +1,20 @@
-
 import assert = require('assert');
-import Sample = require("../main/sample");
+import Sample = require('../main/sample');
 
-describe(" # Sample ",()=>{
-    context(" ## context ",()=>{
+describe(' # Sample ', () => {
+  context(' ## context ', () => {
+    let name = 'tom';
+    let age = 25;
 
-	let name = "tom";
-	let age = 25;
+    let sample = new Sample(name, age);
 
-	let sample = new Sample(name,age);
-
-	it(" ### getName() ", ()=>{
-	    const expectedName = "tom";
-	    assert.ok(sample.getName() === expectedName);
-	});
-	it(" ### getAge() ", ()=>{
-	    const expectedAge = 25;
-	    assert.ok(sample.getAge() === expectedAge);
-	});
+    it(' ### getName() ', () => {
+      const expectedName = 'tom';
+      assert.ok(sample.getName() === expectedName);
     });
+    it(' ### getAge() ', () => {
+      const expectedAge = 25;
+      assert.ok(sample.getAge() === expectedAge);
+    });
+  });
 });
