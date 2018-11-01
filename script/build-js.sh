3eaf5eb049e89dcd5bd9de5c09feeaa19a5316c1
gumedev <gume.mail.git@gmail.com>
2018-11-01 09:33:15 +0900

    #5 Add uglify-js sample

--- script/build-js.sh
+++ script/build-js.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
 Path_browserify=./node_modules/.bin/browserify
+Path_uglifyjs=./node_modules/.bin/uglifyjs
+
+function make_bundle_dist(){
+
+    local browserify_base_file=./src/js/main/main.js
+    local browserify_src_file=./src/js/main/main-dist.js
+    local browserify_dest_file=./src/js/main/bundle-dist.js
+
+    echo 'window.console.log = function(i){ return;};' > ${browserify_src_file}
+    cat ${browserify_base_file} >> ${browserify_src_file}
+
+    ${Path_browserify} -o ${browserify_dest_file} ${browserify_src_file}
+}
+
+function uglify_js(){
+
+    local src_file=./src/js/main/bundle-dist.js
+    local dest_file=./js/bundle.min.js
+
+    ${Path_uglifyjs} --compress --mangle -o ${dest_file} ${src_file}
+}
 
 function main(){
 
     ${Path_browserify} -d -o ./src/js/main/bundle.js ./src/js/main/main.js
+
+    make_bundle_dist
+    uglify_js
 }
 main
--- src/js/main/main.js
+++ src/js/main/main.js
@@ -1,8 +1,8 @@
-const Sample = require("./sample");
-const Printer = require("./printer");
+var Sample = require("./sample");
+var Printer = require("./printer");
 
-let sample = new Sample("tom",25);
+var sample = new Sample("tom",25);
 console.log(sample);
 
-let printer = new Printer("hello world","+");
+var printer = new Printer("hello world","+");
 console.log(printer);
