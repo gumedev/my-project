9c43ae04fdf5a65287ea03e73ae9bdfa8671a8a6
gumedev <gume.dev.mail+github@gmail.com>
2018-12-04 23:13:41 +0900

    #6 Add html-format-sample with prettier
     prettier-1.15.0^ からHTML整形サポートが追加された

--- script/build-ejs.sh
+++ script/build-ejs.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
 Path_ejs_cli=./node_modules/.bin/ejs-cli
+Path_prettier=./node_modules/.bin/prettier
 Src_path=./src/ejs
+
+function ejs_cli(){
+
+    echo "@@ build-ejs.sh > ejs_cli"
+    ${Path_ejs_cli} --base-dir ${Src_path} "*.ejs" --out ./
+}
+
+function prettier_html(){
+
+    echo "@@ build-ejs.sh > prettier_html"
+    ${Path_prettier} --parser html --print-width 100 ./src/ejs/template.html
+}
 
 function main(){
 
-    echo "@@ build-ejs.sh > main"
-    ${Path_ejs_cli} --base-dir ${Src_path} "*.ejs" --out ./
+    ejs_cli
+    prettier_html
 }
 main
